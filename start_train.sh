2232f2c1b6a018e06d85afa7ead16d1fdb1ee30e
Lseoksee <da864268@naver.com>
2024-12-02 21:45:34 +0900

    학습코드도

--- start_train.sh
+++ start_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #INFO: 모델 학습을 시키는 스크립트임
-source ~/miniconda3/etc/profile.d/conda.sh
+source /miniconda3/etc/profile.d/conda.sh
 conda activate competition_cuda
 
 python rl_trainer/main.py  --map all --gui false --train --max_episodes 1500
