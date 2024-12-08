5cb399ba0314859f4dd149071b98f47a844ec8ba
Lseoksee <da864268@naver.com>
2024-12-08 23:00:51 +0900

    로그 스크립트 업뎃

--- log.sh
+++ log.sh
@@ -4,14 +4,10 @@ list=$(docker ps --format "table {{.Names}}")
 # 로그 저장 폴더
 loc="./log"
 
-# 마지막 최고기록 저장 파일 이름
-latest_file="$loc/latest.log"
-
 if [ ! -d $loc ] ; then
  mkdir $loc
 fi
 
-echo "" > $latest_file
 for item in $list
 do
     if [[ "$item" == "competition_run"* ]]; then
@@ -19,6 +15,6 @@ do
         # 컨테이너 로그 저장
         docker logs $item >& $loc/$ct_name.log
         # 각 컨테이너 별 최고 기록 저장
-        echo "[$ct_name]" $(docker logs $item 2> /dev/null | grep "현재 최고기록" | tail -n1) >> $latest_file
+        echo "[$ct_name]" $(docker logs $item 2> /dev/null | grep "현재 최고기록" | tail -n1)
     fi
 done
